d56dd2fb450c72d0a50627c690988556e224346f
Kendal Gates <leoneusg@yahoo.com>
2017-09-24 17:23:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,3 +16,11 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array) {
+  array.slice(n)
+  return array
+}
