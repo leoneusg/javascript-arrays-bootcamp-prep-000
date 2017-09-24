badc3ff8e4b916bcf03f4ddcc6ecf4e02d7236a8
Kendal Gates <leoneusg@yahoo.com>
2017-09-24 17:25:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,10 +17,10 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+  array = array.shift()
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  array = array.slice(1)
   return array
 }
